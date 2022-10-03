27f46460fd0a455f3a13a22cc62a01a2cf6d165f
Aswin Gopinathan <aswingopinathan@gmail.com>
2022-10-03 13:08:11 +0530

    youtube integrated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
 import RowPost from './Components/RowPost/RowPost';
-
+import {originals,action} from './url'
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={action} title='Action' isSmall/>
+
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '5b6cdbbfbe401ab6a59f6d732200627f';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
